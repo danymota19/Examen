f0cc62f38b04200e8436dbc270fbc78c48e6dd78
Agustin Castaneda <Agustin@MacBook.local>
2015-05-21 08:53:30 -0500

    tab views controller

---
+++ Examen/Declarations.h
@@ -0,0 +1,13 @@
+//
+//  Declarations.h
+//  Examen
+//
+//  Created by Agustin Castaneda on 21/05/15.
+//  Copyright (c) 2015 Agustin Castaneda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Declarations : NSObject
+
+@end
---
+++ Examen/Declarations.m
@@ -0,0 +1,13 @@
+//
+//  Declarations.m
+//  Examen
+//
+//  Created by Agustin Castaneda on 21/05/15.
+//  Copyright (c) 2015 Agustin Castaneda. All rights reserved.
+//
+
+#import "Declarations.h"
+
+@implementation Declarations
+
+@end
